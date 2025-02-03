0b82ebadb6c2e0d8e2fed0a9d60510044b62925b
Amartya <kamartya58@gmail.com>
2025-02-04 01:19:57 +0530

    1) Implemented AnimatedContainer to provide animation when user click on the login button 2) Implemented Stateful widget to reflect realtime change or (render the ui every time the state changes) 3) used Future.delayed() to delay the Navigator for few seconds.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tech_cart/utils/MyRoutes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+    String name = "";
+    bool isChanged = false;
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
@@ -17,7 +25,7 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 40,
                 ),
-                Text("Welcome",
+                Text("Welcome $name",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold
@@ -33,6 +41,11 @@ class LoginPage extends StatelessWidget {
                           hintText: "Enter UserName",
                           labelText: "UserName"
                       ),
+                      // to reflect the change in the state to the Welcome text
+                      onChanged: (value){
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -44,14 +57,45 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
-                        onPressed:() {
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                          // print("Tech Chart");
-                        },
-                        child: Text("Log In"),
-                      style: TextButton.styleFrom(minimumSize: Size(200, 50)),
+
+                    // Animated Container (Button)
+                    InkWell(
+                      onTap: () async{
+                        setState(() {
+                          isChanged = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: isChanged ? 50 : 200,
+                        height: 50,
+                        // color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(isChanged ? 50 : 10),
+                        ),
+                        child:isChanged? Icon(Icons.done, color: Colors.white,): Text(
+                          "Log In",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: GoogleFonts.lato().fontFamily,
+                              color: Colors.white,),
+                        ),
+
+                      ),
                     ),
+                    // ElevatedButton(
+                    //     onPressed:() {
+                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //       // print("Tech Chart");
+                    //     },
+                    //     child: Text("Log In"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(200, 50)),
+                    // ),
                   ],),
                 )
 
